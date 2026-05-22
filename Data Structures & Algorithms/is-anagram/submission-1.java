ed84943b195d2afb1ececd0042fd935f138ed44f
auyondebnath-cpu <auyon.debnath@students.mq.edu.au>
2026-05-22 23:05:34 +1000

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length()) return false;
+        int[] set = new int[26];
+
+        for(int i =0; i<s.length(); i++){
+            set[s.charAt(i)-'a']++;
+            set[t.charAt(i)-'a']--;
+        }
+
+        for(int c:set){
+            if(c!=0) return false;
+        }
+        return true;
+    }
+}
